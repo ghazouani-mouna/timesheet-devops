c2aff130013ca52b54820f979da37d291dc694d5
YassineAdalet <yassine.adalet@esprit.tn>
2021-11-04 22:36:32 +0100

    MissionService Added

---
+++ Timesheet-spring-boot-core-data-jpa-mvc-REST-1/src/main/java/tn/esprit/spring/services/MissionServiceImpl.java
@@ -0,0 +1,60 @@
+package tn.esprit.spring.services;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import tn.esprit.spring.entities.Departement;
+import tn.esprit.spring.entities.Employe;
+import tn.esprit.spring.entities.Mission;
+import tn.esprit.spring.entities.Role;
+import tn.esprit.spring.entities.Timesheet;
+import tn.esprit.spring.entities.TimesheetPK;
+import tn.esprit.spring.repository.DepartementRepository;
+import tn.esprit.spring.repository.EmployeRepository;
+import tn.esprit.spring.repository.MissionRepository;
+import tn.esprit.spring.repository.TimesheetRepository;
+
+@Service
+public class MissionServiceImpl implements ITimesheetService {
+	
+
+	@Autowired
+	MissionRepository missionRepository;
+	@Autowired
+	DepartementRepository deptRepoistory;
+	@Autowired
+	TimesheetRepository timesheetRepository;
+	@Autowired
+	EmployeRepository employeRepository;
+	
+	public int ajouterMission(Mission mission) {
+		missionRepository.save(mission);
+		return mission.getId();
+	}
+    
+	public void affecterMissionADepartement(int missionId, int depId) {
+		Mission mission = missionRepository.findById(missionId).get();
+		Departement dep = deptRepoistory.findById(depId).get();
+		mission.setDepartement(dep);
+		missionRepository.save(mission);
+		
+	}
+
+	
+
+	
+	public List<Mission> findAllMissionByEmployeJPQL(int employeId) {
+		return timesheetRepository.findAllMissionByEmployeJPQL(employeId);
+	}
+
+	
+	public List<Employe> getAllEmployeByMission(int missionId) {
+		return timesheetRepository.getAllEmployeByMission(missionId);
+	}
+
+}
