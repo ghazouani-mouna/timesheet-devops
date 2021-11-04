cfdd7a6f0c0cc01a97604d88ceb9a6b2b628b713
lamis-barbouch <lamis.barbouch@esprit.tn>
2021-11-04 15:02:12 +0100

    Correction de l'interface IDepartementService

--- Timesheet-spring-boot-core-data-jpa-mvc-REST-1/src/main/java/tn/esprit/spring/services/IDepartementService.java
+++ Timesheet-spring-boot-core-data-jpa-mvc-REST-1/src/main/java/tn/esprit/spring/services/IDepartementService.java
@@ -5,7 +5,6 @@ import java.util.List;
 import tn.esprit.spring.entities.Departement;
 import tn.esprit.spring.entities.Entreprise;
 
-
 public interface IDepartementService {
 	public int ajouterDepartement(Departement dep);
 	void affecterDepartementAEntreprise(int depId, int entrepriseId);
