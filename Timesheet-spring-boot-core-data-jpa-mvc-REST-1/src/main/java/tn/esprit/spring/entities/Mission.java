e05ccc361d50c0415bf58cd9051e9e0b724c48bf
ghazouani-mouna <mouna.ghazouani@esprit.tn>
2021-11-08 22:49:05 +0100

    testing

--- Timesheet-spring-boot-core-data-jpa-mvc-REST-1/src/main/java/tn/esprit/spring/entities/Mission.java
+++ Timesheet-spring-boot-core-data-jpa-mvc-REST-1/src/main/java/tn/esprit/spring/entities/Mission.java
@@ -10,7 +10,7 @@ import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
-@Entity
+@Entity 
 public class Mission implements Serializable {
 
 	private static final long serialVersionUID = -5369734855993305723L;
